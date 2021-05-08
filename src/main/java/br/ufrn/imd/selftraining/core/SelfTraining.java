e42c67e94a25435dcbe82fd4e4ccf03b9d76f6df
Cephas Barreto <cephasax@gmail.com>
2021-05-08 14:58:54 -0300

    clean project

--- src/main/java/br/ufrn/imd/selftraining/core/SelfTraining.java
+++ src/main/java/br/ufrn/imd/selftraining/core/SelfTraining.java
@@ -2,7 +2,6 @@ package br.ufrn.imd.selftraining.core;
 
 import br.ufrn.imd.selftraining.results.FoldResult;
 import weka.classifiers.Classifier;
-import weka.classifiers.functions.MultilayerPerceptron;
 import weka.classifiers.trees.J48;
 import weka.core.Instances;
 
@@ -17,27 +16,27 @@ public class SelfTraining {
 
 	protected int unlabeledSetJoinRate = 10;
 	protected int amountToJoin = 0;
-	
+
 	protected Dataset tempSet;
 
 	protected Classifier mainClassifier;
 
 	protected int goodClassifiedInstances = 0;
 	protected int missClassifiedInstances = 0;
-	
+
 	protected FoldResult result;
 	protected String history;
 	protected String iterationInfo;
-	
+
 	public SelfTraining() {
-		
+
 	}
-	
+
 	public SelfTraining(Dataset testSet, Dataset validationSet) {
-		
+
 		this.result = new FoldResult();
 		this.history = new String();
-		
+
 		this.validationSet = new Dataset(validationSet);
 		this.testSet = new Dataset(testSet);
 		this.tempSet = new Dataset(testSet);
@@ -46,7 +45,7 @@ public class SelfTraining {
 		splitDatasetStratified();
 		createMainClassifier();
 	}
-	
+
 	protected void splitDatasetStratified() {
 		testSet.getInstances().stratify(10);
 		this.labeledSet = new Dataset(testSet.getInstances().testCV(10, 0));
@@ -64,85 +63,78 @@ public class SelfTraining {
 		for (int i = total; i < this.testSet.getInstances().size(); i++) {
 			this.unlabeledSet.getInstances().add(this.testSet.getInstances().get(i));
 		}
-	}	
-	
+	}
+
 	protected void trainMainCLassifierOverLabeledSet() throws Exception {
 		this.mainClassifier.buildClassifier(this.labeledSet.getInstances());
 	}
-	
+
 	protected void clearTempSet() {
 		this.tempSet.getInstances().clear();
 	}
-	
+
 	protected void createMainClassifier() {
+		
 		// weka.classifiers.trees.J48 -C 0.05 -M 2 (74.4792)
-		/*
-		 * J48 j48 = new J48(); try {
-		 * j48.setOptions(weka.core.Utils.splitOptions("-C 0.05 -M 2")); } catch
-		 * (Exception e) { e.printStackTrace(); } this.mainClassifier = (J48) j48;
-		 */
-		
-		//new for test - 26/01/2020
-		//weka.classifiers.functions.MultilayerPerceptron -L 0.3 -M 0.2 -N 500 -V 0 -S 0 -E 20 -H a
-		
-		MultilayerPerceptron mlp = new MultilayerPerceptron();
+		J48 j48 = new J48();
 		try {
-			mlp.setOptions(weka.core.Utils.splitOptions("-L 0.3 -M 0.2 -N 500 -V 0 -S 0 -E 20 -H a"));
+			j48.setOptions(weka.core.Utils.splitOptions("-C 0.05 -M 2"));
 		} catch (Exception e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		this.mainClassifier = mlp;
+		this.mainClassifier = (J48) j48;
+
 	}
-	
+
 	protected void joinClassifiedWithLabeledSet() {
 		labeledSet.getInstances().addAll(this.tempSet.getInstances());
 	}
-	
-	protected void mainClassifierJob() throws Exception{
-		Measures measures = new Measures(this.mainClassifier, this.labeledSet.getInstances(), this.validationSet.getInstances());
-		
+
+	protected void mainClassifierJob() throws Exception {
+		Measures measures = new Measures(this.mainClassifier, this.labeledSet.getInstances(),
+				this.validationSet.getInstances());
+
 		result.setAccuracy(measures.getAccuracy());
 		result.setError(measures.getError());
 		result.setfMeasure(measures.getFmeasureMean());
-		result.setPrecision(measures.getPrecisionMean()); 
-		result.setRecall(measures.getRecallMean()); 	
+		result.setPrecision(measures.getPrecisionMean());
+		result.setRecall(measures.getRecallMean());
 	}
-	
+
 	protected void printIterationInfo() {
 		System.out.println(this.iterationInfo);
 	}
-	
+
 	protected void addIterationInfoToHistory() {
 		addToHistory(this.iterationInfo);
 	}
-	
+
 	protected void addToHistory(String string) {
 		StringBuilder sb = new StringBuilder();
 		sb.append(this.history);
 		sb.append(string);
-		
+
 		this.history = new String(sb.toString());
 	}
-	
+
 	protected void generateIterationInfo(int iteration) {
-		
+
 		StringBuilder sb = new StringBuilder();
-		
+
 		sb.append("--------------------------------------------------\n");
 		sb.append("@ SELFTRAINING ITERATION: " + iteration + "\n");
 		sb.append("--------------------------------------------------\n");
 		sb.append("\n");
 		sb.append("@ LABELED: " + this.labeledSet.getInstances().size() + "\n");
 		sb.append("@ UNLABELED: " + this.unlabeledSet.getInstances().size() + "\n");
-		if(iteration > 1) {
+		if (iteration > 1) {
 			sb.append("@ From unlabeled to labeled (last iteration): " + goodClassifiedInstances + "\n");
 		}
 		sb.append("--------------------------------------------------\n");
 		this.iterationInfo = new String(sb.toString());
 	}
-	
-	//GETTERS AND SETTERS 
+
+	// GETTERS AND SETTERS
 	public Dataset getValidationSet() {
 		return validationSet;
 	}
